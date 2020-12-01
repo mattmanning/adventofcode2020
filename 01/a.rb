61b46ffa297a629229236878d0829c11906482cb
Matt Manning <matt.manning@gmail.com>
2020-12-01 17:33:29 -0500

    executable

--- 01/a.rb
+++ 01/a.rb
@@ -1,1 +1,4 @@
+ #!/usr/bin/env ruby
+
  puts File.open('input').map(&:to_i).combination(2).detect { |c| c.sum == 2020 }.inject(&:*)
+ 
--- 01/b.rb
+++ 01/b.rb
@@ -1,1 +1,3 @@
+ #!/usr/bin/env ruby
+
  puts File.open('input').map(&:to_i).combination(3).detect { |c| c.sum == 2020 }.inject(&:*)
