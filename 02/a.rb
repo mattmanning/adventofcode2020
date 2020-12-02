5bd342e6456b2af0517b069458915672cf8b7c35
Matt Manning <matt.manning@gmail.com>
2020-12-02 16:02:58 -0500

    fix whitespace

--- 02/a.rb
+++ 02/a.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 total = File.open('input').inject(0) do |sum, line|
-	cols = line.split(' ')
-	range = Range.new(*cols[0].split('-').map(&:to_i))
-	letter = cols[1].split(':')[0]
-	appearances = cols[2].split('').count(letter)
-	if range.cover?(appearances)
-		sum + 1
-	else
-		sum
-	end
+  cols = line.split(' ')
+  range = Range.new(*cols[0].split('-').map(&:to_i))
+  letter = cols[1].split(':')[0]
+  appearances = cols[2].split('').count(letter)
+  if range.cover?(appearances)
+    sum + 1
+  else
+    sum
+  end
 end
 
 puts total
