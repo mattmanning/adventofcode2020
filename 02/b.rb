d43a92be28b086c60c9735c5671bfca030e94403
Matt Manning <matt.manning@gmail.com>
2020-12-03 19:44:08 -0500

    remove unneeded line

--- 02/b.rb
+++ 02/b.rb
@@ -4,7 +4,6 @@ total = File.open('input').inject(0) do |sum, line|
   cols = line.split(' ')
   positions = cols[0].split('-').map(&:to_i)
   letter = cols[1].split(':')[0]
-  appearances = cols[2].split('').count(letter)
   if (cols[2][positions[0]-1] == letter) ^ (cols[2][positions[1]-1] == letter)
     sum + 1
   else
