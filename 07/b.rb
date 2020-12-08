84cab787946417028c5f75cfc1653831e84de14e
Matt Manning <matt.manning@gmail.com>
2020-12-07 23:42:41 -0500

    Day 7

--- 07/b.rb
+++ 07/b.rb
@@ -5,21 +5,22 @@ File.read('input').split("\n").each do |line|
   outer = split[0]
   inner = split[1][0..-2].split(', ')
   inner.each do |bag|
-    color = /\d\s(.*)\sbag/.match(bag).to_a[1]
-    bag_hash[color] = (bag_hash[color] + [outer]).uniq
+    m = /(\d+)\s(.*)\sbag/.match(bag).to_a
+    next if m.empty?
+    bag_hash[outer] = (bag_hash[outer] + [[m[2], m[1].to_i]])
   end
 end
 
 sgb = bag_hash['shiny gold']
+
 counter = 0
 counted = []
 
 while !sgb.empty?
-  sgb += bag_hash[sgb.first]
-  sgb.uniq!
-  sgb -= counted
+  sgb += bag_hash[sgb.first.first].map {|b| [b.first, b.last*sgb.first.last]}
+  puts sgb.inspect
+  counter += sgb.first.last
   counted << sgb.shift
-  counter += 1
 end
 
 puts counter
