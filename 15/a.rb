aa4c8b4f837292d856593bb9534b9a5c998558f2
Matt Manning <matt.manning@gmail.com>
2020-12-15 13:19:20 -0500

    Day 15

---
+++ 15/a.rb
@@ -0,0 +1,17 @@
+input = 9,19,1,6,0,5,4
+loops = 2020
+h = Hash.new([])
+
+input.each_with_index {|n,i| h[n] = [i+1]}
+
+last = input.last
+count = input.length
+
+while count < loops
+  h[last] = [count] + h[last]
+  h[last] = h[last][0..1] if h[last].length > 2
+  last = h[last].first - h[last].last
+  count += 1
+end
+
+puts last
